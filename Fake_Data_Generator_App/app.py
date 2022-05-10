e5629de19ac04e96a7486248b0254296d1753985
vanitha159105 <104796501+vanitha159105@users.noreply.github.com>
2022-05-09 22:21:48 -0700

    Update app.py

--- Fake_Data_Generator_App/app.py
+++ Fake_Data_Generator_App/app.py
@@ -144,9 +144,25 @@ def main():
 		
 
 	else:
-		df = pd.DataFrame(data_df)
-                df
+		# Create the data of the DataFrame as a dictionary
+data_df = {'Name': ['Asha', 'Harsh', 'Sourav', 'Riya', 'Hritik',
+                    'Shivansh', 'Rohan', 'Akash', 'Soumya', 'Kartik'],
 
+           'Department': ['Administration', 'Marketing', 'Technical', 'Technical', 'Marketing',
+                          'Administration', 'Technical', 'Marketing', 'Technical', 'Administration'],
+
+           'Employment Type': ['Full-time Employee', 'Intern', 'Intern', 'Part-time Employee', 'Part-time Employee',
+                               'Full-time Employee', 'Full-time Employee', 'Intern', 'Intern', 'Full-time Employee'],
+
+           'Salary': [120000, 50000, 70000, 70000, 55000,
+                      120000, 125000, 60000, 50000, 120000],
+
+           'Years of Experience': [5, 1, 2, 3, 4,
+                                   7, 6, 2, 1, 6]}
+
+# Create the DataFrame
+df = pd.DataFrame(data_df)
+df
 	       
                
 
