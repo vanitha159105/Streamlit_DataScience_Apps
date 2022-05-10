977c13a839fe6cf18a1d9f2fcdc927224669a7ed
vanitha159105 <104796501+vanitha159105@users.noreply.github.com>
2022-05-09 22:20:16 -0700

    Update app.py

--- Fake_Data_Generator_App/app.py
+++ Fake_Data_Generator_App/app.py
@@ -144,11 +144,9 @@ def main():
 		
 
 	else:
-	       df = pd.DataFrame({'key': ['A', 'B', 'C', 'A', 'B', 'C'],
-                   'data1': range(6),
-                   'data2': rng.randint(0, 10, 6)},
-                   columns = ['key', 'data1', 'data2'])
-               df
+		df = pd.DataFrame(data_df)
+df
+
 	       
                
 
