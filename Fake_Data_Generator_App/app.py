c1aa20914d3097a34fe2a7004f55fbf8c30a4a18
vanitha159105 <104796501+vanitha159105@users.noreply.github.com>
2022-05-10 22:40:48 -0700

    Update app.py

--- Fake_Data_Generator_App/app.py
+++ Fake_Data_Generator_App/app.py
@@ -93,7 +93,6 @@ def main():
 		localized_providers = ["ar_AA", "ar_EG", "ar_JO", "ar_PS", "ar_SA", "bg_BG", "bs_BA", "cs_CZ", "de", "de_AT", "de_CH", "de_DE", "dk_DK", "el_CY", "el_GR", "en", "en_AU", "en_CA", "en_GB", "en_IE", "en_IN", "en_NZ", "en_PH", "en_TH", "en_US", "es", "es_CA", "es_ES", "es_MX", "et_EE", "fa_IR", "fi_FI", "fil_PH", "fr_CA", "fr_CH", "fr_FR", "fr_QC", "he_IL", "hi_IN", "hr_HR", "hu_HU", "hy_AM", "id_ID", "it_CH", "it_IT", "ja_JP", "ka_GE", "ko_KR", "la", "lb_LU", "lt_LT", "lv_LV", "mt_MT", "ne_NP", "nl_BE", "nl_NL", "no_NO", "or_IN", "pl_PL", "pt_BR", "pt_PT", "ro_RO", "ru_RU", "sk_SK", "sl_SI", "sv_SE", "ta_IN", "th", "th_TH", "tl_PH", "tr_TR", "tw_GH", "uk_UA", "zh_CN", "zh_TW"]
 		locale = st.sidebar.multiselect("Select Locale",localized_providers,default="en_US")
 		dataformat = st.sidebar.selectbox("Save Data As",["csv","json"])
-
 		scores = {'Zone': ['North','South','South',
                    'East','East','West','West','West','West'], 
           'School': ['Rushmore','Bayside','Rydell',
@@ -103,9 +102,12 @@ def main():
                    'Jimmy','Erik','Lam','Yip','Chen','Jim'], 
           'Math': [78,76,56,67,89,100,55,76,79],
           'Science': [70,68,90,45,66,89,32,98,70]}
-              df = pd.DataFrame(scores, columns = 
+		df = pd.DataFrame(scores, columns = 
                   ['Zone', 'School', 'Name', 
                    'Science', 'Math'])
+
+
+		
 		st.dataframe(df)
 		with st.beta_expander("📩: Download"):
 			make_downloadable_df_format(df,dataformat)
