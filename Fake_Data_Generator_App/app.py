4cd12e8a69c85c26142ee1cbc3ba435f013684c9
vanitha159105 <104796501+vanitha159105@users.noreply.github.com>
2022-05-10 23:32:45 -0700

    Update app.py

--- Fake_Data_Generator_App/app.py
+++ Fake_Data_Generator_App/app.py
@@ -90,8 +90,7 @@ def main():
 	if choice == "Expense Category and Cust Life by EG":
 		st.subheader("Expense Category and Cust Life by EG")
 		dataformat = st.sidebar.selectbox("Fiscal Year",["2021","2022"])
-		localized_providers = ["ar_AA", "ar_EG", "ar_JO", "ar_PS", "ar_SA", "bg_BG", "bs_BA", "cs_CZ", "de", "de_AT", "de_CH", "de_DE", "dk_DK", "el_CY", "el_GR", "en", "en_AU", "en_CA", "en_GB", "en_IE", "en_IN", "en_NZ", "en_PH", "en_TH", "en_US", "es", "es_CA", "es_ES", "es_MX", "et_EE", "fa_IR", "fi_FI", "fil_PH", "fr_CA", "fr_CH", "fr_FR", "fr_QC", "he_IL", "hi_IN", "hr_HR", "hu_HU", "hy_AM", "id_ID", "it_CH", "it_IT", "ja_JP", "ka_GE", "ko_KR", "la", "lb_LU", "lt_LT", "lv_LV", "mt_MT", "ne_NP", "nl_BE", "nl_NL", "no_NO", "or_IN", "pl_PL", "pt_BR", "pt_PT", "ro_RO", "ru_RU", "sk_SK", "sl_SI", "sv_SE", "ta_IN", "th", "th_TH", "tl_PH", "tr_TR", "tw_GH", "uk_UA", "zh_CN", "zh_TW"]
-		locale = st.sidebar.multiselect("Select Locale",localized_providers,default="en_US")
+		
 		dataformat = st.sidebar.selectbox("Save Data As",["csv","json"])
 		
 
