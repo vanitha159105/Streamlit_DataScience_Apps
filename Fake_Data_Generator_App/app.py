ef9cb8699a19a1f21bb6669f92a5d47a872a3a29
vanitha159105 <104796501+vanitha159105@users.noreply.github.com>
2022-05-10 21:33:48 -0700

    Update app.py

--- Fake_Data_Generator_App/app.py
+++ Fake_Data_Generator_App/app.py
@@ -151,20 +151,7 @@ def main():
 		
 
 	else:
-		# hierarchical indices and columns
-                index = pd.MultiIndex.from_product([[2013, 2014], [1, 2]],
-                                   names=['year', 'visit'])
-                columns = pd.MultiIndex.from_product([['Bob', 'Guido', 'Sue'], ['HR', 'Temp']],
-                                     names=['subject', 'type'])
 
-# mock some data
-data = np.round(np.random.randn(4, 6), 1)
-data[:, ::2] *= 10
-data += 37
-
-# create the DataFrame
-health_data = pd.DataFrame(data, index=index, columns=columns)
-health_data
 
 
                 
