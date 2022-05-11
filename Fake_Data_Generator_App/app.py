dbda2648c9df54857afd866a45a3e3e6144d56c9
vanitha159105 <104796501+vanitha159105@users.noreply.github.com>
2022-05-10 23:24:48 -0700

    Update app.py

--- Fake_Data_Generator_App/app.py
+++ Fake_Data_Generator_App/app.py
@@ -11,7 +11,7 @@ from faker import Faker
 import base64
 import time 
 timestr = time.strftime("%Y%m%d-%H%M%S")
-
+my_string=Capital
 
 # Fxn to Download
 def make_downloadable_df(data):
@@ -93,7 +93,7 @@ def main():
 		localized_providers = ["ar_AA", "ar_EG", "ar_JO", "ar_PS", "ar_SA", "bg_BG", "bs_BA", "cs_CZ", "de", "de_AT", "de_CH", "de_DE", "dk_DK", "el_CY", "el_GR", "en", "en_AU", "en_CA", "en_GB", "en_IE", "en_IN", "en_NZ", "en_PH", "en_TH", "en_US", "es", "es_CA", "es_ES", "es_MX", "et_EE", "fa_IR", "fi_FI", "fil_PH", "fr_CA", "fr_CH", "fr_FR", "fr_QC", "he_IL", "hi_IN", "hr_HR", "hu_HU", "hy_AM", "id_ID", "it_CH", "it_IT", "ja_JP", "ka_GE", "ko_KR", "la", "lb_LU", "lt_LT", "lv_LV", "mt_MT", "ne_NP", "nl_BE", "nl_NL", "no_NO", "or_IN", "pl_PL", "pt_BR", "pt_PT", "ro_RO", "ru_RU", "sk_SK", "sl_SI", "sv_SE", "ta_IN", "th", "th_TH", "tl_PH", "tr_TR", "tw_GH", "uk_UA", "zh_CN", "zh_TW"]
 		locale = st.sidebar.multiselect("Select Locale",localized_providers,default="en_US")
 		dataformat = st.sidebar.selectbox("Save Data As",["csv","json"])
-		x=Capital
+		my_string=Capital
 		y=Expense
 
 		multi_index = pd.MultiIndex.from_tuples([("EG_RENEWALS_EARNINGS","1"),
@@ -105,7 +105,7 @@ def main():
                                   ("May", "CapVsExp"),
                                   ("May", "EG_Customer_Life")])
 		
-		data=[[x,2,y,4], [x,0,y,2]]
+		data=[[my_string,2,y,4], [x,0,y,2]]
 
 		df = pd.DataFrame(data, columns=cols,index=multi_index)
 
